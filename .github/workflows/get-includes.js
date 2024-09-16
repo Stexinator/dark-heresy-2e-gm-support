5051e70c0acfbb6c21cab0804d07b1dad43d3599
Stexinator <hurricane90@gmx.de>
2024-09-16 21:13:07 +0200

    fix release pipeline

--- .github/workflows/get-includes.js
+++ .github/workflows/get-includes.js
@@ -1,2 +1,2 @@
 var fs = require('fs');
-console.log(JSON.parse(fs.readFileSync('src/build.json', 'utf8')).includes.join(" "));
+console.log(JSON.parse(fs.readFileSync('build.json', 'utf8')).includes.join(" "));
